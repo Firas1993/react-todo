bd61615eea63636442cb5ccebcc542ff46c4f69a
Firas1993 <kebsi.firas@competitoor.com>
2019-10-26 10:17:54 +0200

    "main app, function added and props setted"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import uui from 'uuid';
+import uuid from "uuid";
 
-import React from 'react';
+import React, { Component } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import TodoInput from './component/TodoInput';
-import TodoItem from './component/TodoItem';
-import TodoList from './component/TodoList';
-function App() {
-  return (
-    <div className="App">
-    <TodoInput />
-    <TodoItem />
-    <TodoList />
-    </div>
-  );
-}
+import TodoInput from "./component/TodoInput";
+import TodoList from "./component/TodoList";
 
-export default App;
+export default class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id: uuid(),
+    item: "", //pass it down to todoItem
+    editItem: false
+  };
+  handleChange = e => {
+    console.log("handle Change from app.js");
+  };
+
+  handleSubmit = e => {
+    console.log("handle Submit from app.js");
+  };
+  clearList = e => {
+    console.log("clear list from app.js");
+  };
+  handleDelete = (id) => {
+    console.log(`delete item id ${id}`)
+  }
+  handleEdit = (id) =>{
+    console.log(`edit ${id}`)
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">to do input</h3>
+          </div>
+
+          <TodoInput 
+          item={this.state.item}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          editItem={this.state.editItem}
+          />
+          
+          <TodoList 
+          items={this.state.items}
+          clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+          />
+
+        </div>
+      </div>
+    );
+  }
+}
